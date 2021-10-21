52de2b969a8ac833cc1dd3eb07220fd3a61a367a
Alejandra B. C. Glez <86536719+alebea0@users.noreply.github.com>
2021-10-21 14:56:08 -0500

    Agrega el manejo de eventos que da comportamiento a la interfaz de  usuario de la aplicación

--- ContadorManual/ContadorManual/MainPage.xaml.cs
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -10,9 +10,29 @@ namespace ContadorManual
 {
     public partial class MainPage : ContentPage
     {
+        private int _Conteo;
+
         public MainPage()
         {
             InitializeComponent();
+            _Conteo = 0;
+
+        }
+
+        private void ContarButton_Clicked(object sender, EventArgs e)
+        {
+
+            _Conteo++;
+            ConteoLabel.Text = _Conteo.ToString();
+
+        }
+
+        private void ReiniciarButton_Clicked(object sender, EventArgs e)
+        {
+
+            _Conteo = 0;
+            ConteoLabel.Text = _Conteo.ToString();
+
         }
     }
 }
